4a63b7149eed04fc6a7a0052bf2ab1259c6879e7
Viraj <vpshah543@gmail.com>
2026-03-05 16:45:29 +0530

    vm_login

--- AMS/Repositories/Interfaces/IUserInterface.cs
+++ AMS/Repositories/Interfaces/IUserInterface.cs
@@ -4,7 +4,7 @@ namespace Repositories.Interfaces
 {
     public interface IUserInterface
     {
-        public Task<List<t_Employee>> LoginUser(string Email, string Password);
+        public Task<List<t_Employee>> LoginUser(vm_login login);
 
         public Task<int> RegisterUser(t_Employee employee);
     }
