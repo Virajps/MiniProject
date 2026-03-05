b206ed12fec913186deec60d89f4cc8f243e64db
Viraj <vpshah543@gmail.com>
2026-03-05 16:59:09 +0530

    User Controller error

--- AMS/Repositories/Interfaces/IUserInterface.cs
+++ AMS/Repositories/Interfaces/IUserInterface.cs
@@ -4,7 +4,7 @@ namespace Repositories.Interfaces
 {
     public interface IUserInterface
     {
-        public Task<List<t_Employee>> LoginUser(vm_login login);
+        public Task<t_Employee> LoginUser(vm_login login);
 
         public Task<int> RegisterUser(t_Employee employee);
     }
