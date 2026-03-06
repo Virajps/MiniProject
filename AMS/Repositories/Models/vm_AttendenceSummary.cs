e1f0a50f23d0cc55ca9eed36a1f355460ea60cd3
Palak Agrawal <agrawalspalak@gmail.com>
2026-03-06 11:42:52 +0530

    model create

---
+++ AMS/Repositories/Models/vm_AttendenceSummary.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Repositories.Models;
+namespace Repositories;
+
+public class vm_AttendenceSummary
+{
+      public int EmpId { get; set; }
+        public string? Name { get; set; }
+        public string? Image { get; set; }
+        public string? Email { get; set; }
+        public string? Gender { get; set; }
+
+        public int PresentCount { get; set; }
+        public int AbsentCount { get; set; }
+        public int TotalWorkingHours { get; set; }
+        public int LateInCount { get; set; }
+        public int EarlyOutCount { get; set; }
+
+        public List<vm_TaskSummary> TaskSummaries { get; set; } = new();
+        public List<t_Attendance> AttendanceHistory { get; set; } = new();
+
+        // For today's attendance
+        public t_Attendance? TodayRecord { get; set; }
+}
