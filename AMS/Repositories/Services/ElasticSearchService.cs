847948c3994aea0dbe7ed01abdb6ba8ed861c7ef
JK-345 <cse.220840131043@gmail.com>
2026-03-19 14:59:02 +0530

    ElasticSearch Load Data

--- AMS/Repositories/Services/ElasticSearchService.cs
+++ AMS/Repositories/Services/ElasticSearchService.cs
@@ -1,5 +1,6 @@
 using Elastic.Clients.Elasticsearch;
 using Microsoft.Extensions.Configuration;
+using Repositories.Models;
 
 namespace Repositories.Services
 {
@@ -11,62 +12,67 @@ namespace Repositories.Services
         {
             _indexName = configuration["Elasticsearch:DefaultIndex"]; _client = client;
         }
-    //     public async Task<int> CreateIndexAsync()
-    //     {
-    //         var indexExistsResponse = await _client.Indices.ExistsAsync(_indexName);
-    //         if (!indexExistsResponse.Exists)
-    //         {
-    //             var createIndexResponse = await _client.Indices.CreateAsync<t_Contact>(index => index
-    //             .Index(_indexName)
-    //             .Mappings(mappings => mappings
-    //             .Properties(properties => properties
-    //             .IntegerNumber(x => x.c_ContactId!)
-    //             .IntegerNumber(x => x.c_UserId)
-    //             .Text(x => x.c_ContactName)
-    //             .Keyword(x => x.c_Email)
-    //             .Keyword(x => x.c_Address)
-    //             .Keyword(x => x.c_Mobile)
-    //             .Keyword(x => x.c_Status)
-    //             .Keyword(x => x.c_Group)
-    //             .Keyword(x => x.c_Image))
-    //             )
-    //             );
-    //             if (!createIndexResponse.IsValidResponse)
-    //             {
-    //                 Console.WriteLine($"Failed to create index: {createIndexResponse.DebugInformation}");
-    //                 return -1;
-    //             }
-    //             Console.WriteLine("Contacts index created successfully.");
-    //             return 1;
-    //         }
-    //         else
-    //         {
-    //             Console.WriteLine("Contacts index already exists.");
-    //             return 0;
-    //         }
-    //     }
-    //     public async Task IndexContactAsync(t_Contact contact)
-    //     {
-    //         var response = await _client.IndexAsync(contact, idx => idx.Index(_indexName));
-    //         if (!response.IsValidResponse)
-    //         {
-    //             throw new Exception($"Error indexing contact: {response.DebugInformation}");
-    //         }
-    //     }
-    //     public async Task<List<t_Contact?>> SearchContactNameAsync(string name)
-    //     {
-    //         var response = await _client.SearchAsync<t_Contact>(s => s
-    //         .Query(q => q.MatchPhrasePrefix(m => m
-    //             .Field(f => f.c_ContactName)
-    //             .Query(name)
-    //         ))
-    //         );
-    //         if (response == null || response.Documents == null)
-    //         {
-    //             Console.WriteLine(" ElasticSearch query returned null or invalid response.");
-    //             return null;
-    //         }
-    //         return response.Documents.ToList();
-    //     }
+        public async Task<int> CreateIndexAsync()
+        {
+            var indexExistsResponse = await _client.Indices.ExistsAsync(_indexName);
+            if (!indexExistsResponse.Exists)
+            {
+                var createIndexResponse = await _client.Indices.CreateAsync<t_Attendance>(index => index
+                .Index(_indexName)
+                .Mappings(mappings => mappings
+                .Properties(properties => properties
+                .IntegerNumber(x => x.AttendId!)
+                .IntegerNumber(x => x.EmpId)
+                .Date(x => x.AttendDate)
+                .IntegerNumber(x => x.ClockInHour)
+                .IntegerNumber(x => x.ClockInMin)
+                .IntegerNumber(x => x.ClockOutHour)
+                .IntegerNumber(x => x.ClockOutMin)
+                .IntegerNumber(x => x.WorkingHour)
+                .Keyword(x => x.TaskType)
+                .Keyword(x => x.WorkType)
+                .Keyword(k => k.Name(f => f.Role))
+                    .Keyword(k => k.Name(f => f.Status))
+                    .Keyword(k => k.Name(f => f.AttendStatus))
+                .Keyword(x => x.EmployeeName))
+                )
+                );
+                if (!createIndexResponse.IsValidResponse)
+                {
+                    Console.WriteLine($"Failed to create index: {createIndexResponse.DebugInformation}");
+                    return -1;
+                }
+                Console.WriteLine("Attendance index created successfully.");
+                return 1;
+            }
+            else
+            {
+                Console.WriteLine("Attendance index already exists.");
+                return 0;
+            }
+        }
+        public async Task IndexAttendanceAsync(t_Attendance Attend)
+        {
+            var response = await _client.IndexAsync(Attend, idx => idx.Index(_indexName));
+            if (!response.IsValidResponse)
+            {
+                throw new Exception($"Error indexing Attendance: {response.DebugInformation}");
+            }
+        }
+        public async Task<List<t_Attendance?>> SearchAttendanceEmployeeNameAsync(string name)
+        {
+            var response = await _client.SearchAsync<t_Attendance>(s => s
+            .Query(q => q.MatchPhrasePrefix(m => m
+                .Field(f => f.EmployeeName)
+                .Query(name)
+            ))
+            );
+            if (response == null || response.Documents == null)
+            {
+                Console.WriteLine(" ElasticSearch query returned null or invalid response.");
+                return null;
+            }
+            return response.Documents.ToList();
+        }
     }
 }
